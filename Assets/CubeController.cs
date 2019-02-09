46c8976a7c0739fa1ac9d9019482723f6ffc73bf
gravity909 <gravity909@gmail.com>
2019-02-09 21:32:53 +0900

    GoForwardUnityChanプロジェクト
    2019/02/09

---
+++ Assets/CubeController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour
+{
+
+    // キューブの移動速度
+    private float speed = -0.2f;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "CubePrefabTag"　|| collision.gameObject.tag == "groundTag")
+        {
+            // ボックス接触時にSEを流す
+            AudioClip clip = gameObject.GetComponent<AudioSource>().clip;
+            gameObject.GetComponent<AudioSource> ().PlayOneShot (clip);
+           
+        }
+    }
+
+}
